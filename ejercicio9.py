7f13a78eb83de4b55f30de26a696b38bb6935ae1
cesspann <sacesia52@gmail.com>
2026-02-04 10:28:53 -0600

    final

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,10 +1,9 @@
 entrada = input("Ingrese cualquier valor: ")
 
 try:
-  valor_entero = int(entrada)
-  print(valor_entero)
+  entero = int(entrada)
+  print("El valor entero es: ",entero)
 except ValueError:
-  # No se imprime nada si no es un entero, según la solicitud.
   pass
 finally:
   print("Fin de la ejecución")
